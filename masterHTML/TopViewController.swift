08156f6cdea237c07f12490838381e0f77510c40
yukisako <yukisakoatoz@gmail.com>
2016-10-03 19:40:57 +0900

    initial commit

---
+++ masterHTML/TopViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  masterHTML
+//
+//  Created by 迫 佑樹 on 2016/10/03.
+//  Copyright © 2016年 Yuki Sako. All rights reserved.
+//
+
+import UIKit
+
+class TopViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
